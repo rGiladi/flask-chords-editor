bc8abb73b5cb6833e13815cd6a854e5277032f5d
snakemzm <cinfist@gmail.com>
2017-02-05 09:44:09 +0200

    db

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from flask_script import Manager
-from flask_pymongo import PyMongo
+from flask_script import Manager, Shell
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import MigrateCommand, Migrate
 from config import Config
-from datetime import datetime
 from uuid import uuid4
 
-mongo = PyMongo()
-bootstrap = Bootstrap()
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    mongo.init_app(app)
+
+    db.init_app(app)
     bootstrap.init_app(app)
     return app
 
 app = create_app()
 manager = Manager(app)
+migrate = Migrate(app, db)
+
 
 
 @app.route('/')
@@ -26,29 +29,45 @@ def main():
     return render_template('app.html')
 
 
-@app.route('/save_chords', methods=['POST', 'GET'])
+@app.route('/save_chords', methods=['POST'])
 def save_chords():
-    db_chords = mongo.db.chords
-    chords = request.form.get('chords')
-    db_chords.create_index('expiration', expireAfterSeconds=3600*24*3)
-    expiration = {'expiration': datetime.utcnow()}
+    raw_html = request.form.get('chords')
     url = uuid4().hex[:8]
-    db_chords.insert_one({'url': url ,'chords': chords, 'expiration': expiration })
-
+    new_chords = Content(html=raw_html, url=url)
+    db.session.add(new_chords)
     return url
+
 
 @app.route('/chords/<url>', methods=['GET'])
 def get_chords(url):
-    db_chords = mongo.db.chords
-    col = db_chords.find_one({'url': url})
+    col = Content.query.filter_by(url=url).first()
     if col:
-        return render_template('chords.html', chords=col['chords'], url=col['url'])
+        return render_template('chords.html', chords=col.html, url=col.url)
     else:
         return 'הדף המבוקש לא נמצא', 400
+
 
 @app.route('/chords', methods=['POST', 'GET'])
 def chords():
     return render_template('chords.html')
+
+
+# MODELS
+class Content(db.Model):
+    html = db.Column(db.Text)
+    url = db.Column(db.String(9), primary_key=True, unique=True)
+
+    def __init__(self, html, url):
+        self.html = html
+        self.url = url
+
+
+def make_shell_context():
+    return dict(app=app, db=db, Content=Content)
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
 
 if __name__ == '__main__':
     manager.run()
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
+
 class Config():
-    SECRET_KEY = 'e010203AzJQP020200000pq123ABd##FKJ'
-    MONGO_DBNAME = 'chords'
-    MONGO_URI = 'mongodb://localhost:27017/chords'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'e010203AzJQP020200000pq123ABd##FKJ'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://roy:rhcp010203@localhost/mychords'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     TEMPLATES_AUTO_RELOAD = True
 
