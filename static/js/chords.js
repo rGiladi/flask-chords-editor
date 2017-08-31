246834e300aa9ca48bcbbec50def7487acbcacd9
Roy <cinfist@gmail.com>
2017-08-31 04:13:22 +0300

    fixed final-chords extension changing

--- static/js/chords.js
+++ static/js/chords.js
@@ -34,10 +34,11 @@ function changeTone(e) {
     var chords = document.getElementsByClassName('chord_value');
 
     for (i=0; i < chords.length; i++) {
-        
-        var chord_value = /[A-Z]{1}#?/.exec(chords[i].innerHTML);
+    
+        var chord_full = chords[i].innerHTML
+        var chord_value = /[A-Z]{1}#?/.exec(chord_full);
         var chord_index = parseInt(chord_data.indexOf(chord_value[0]));
-        var chord_ext = /\/.*/.exec(chords[i].innerHTML) || '';
+        var chord_ext = chord_full.substr(chord_value.length)
 
         if ( tone > 0) {
             if ( chord_index + tone > 11) {
